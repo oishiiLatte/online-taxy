9341e97e6609cecd4853e915bd2884d00e1dac45
obbiy <krisna.obbi@gmail.com>
2023-07-28 17:53:43 +0700

    - Add presentation logic for ride request view

--- Uber Swift/Core/Trips/Views/RideRequestView.swift
+++ Uber Swift/Core/Trips/Views/RideRequestView.swift
@@ -13,6 +13,7 @@ struct RideRequestView: View {
             Capsule()
                 .foregroundColor(Color(.systemGray5))
                 .frame(width: 48, height: 6)
+                .padding(.top, 8)
             
             // trip info view
             HStack {
@@ -131,9 +132,11 @@ struct RideRequestView: View {
                     .cornerRadius(10)
                     .foregroundColor(.white)
             }
-
+            
         }
+        .padding(.bottom, 24)
         .background(.white)
+        .cornerRadius(16)
     }
 }
 
